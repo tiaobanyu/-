f8b9ff4d3dc3db2e0268cdd374a2fd01ce224d6c
tiaobanyu <tanboyaun666@qq.com>
2025-11-30 21:12:03 +0800

    Add files via upload
    增添了老板类和经理类

---
+++ 职工管理系统/manager.h
@@ -0,0 +1,12 @@
+#pragma once
+using namespace std;
+#include <iostream>
+#include "worker.h"
+//经理类
+class Manager:public Worker{
+    public:
+    Manager(int id,string name,int dId);
+    virtual void showInfo();
+    virtual string getDeptName();
+}
+;
