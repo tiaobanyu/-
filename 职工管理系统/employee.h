34c4aceedd23f74c821e1f5de22063282e3aa8b1
tiaobanyu <tanboyaun666@qq.com>
2025-12-11 15:58:12 +0800

    Delete 职工管理系统 directory

--- 职工管理系统/employee.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-using namespace std;
-#include "worker.h"
-class Employee:public Worker{
-    Employee(int id,string name, int did);
-    virtual void showInfo();
-    virtual string getDeptName();
-};
